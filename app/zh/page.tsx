d76b5dcc5915aafba1c28646c463d7a8a51860a1
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2025-03-22 03:03:13 -0400

    update

--- app/zh/page.tsx
+++ app/zh/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import elysia from "@/public/elysia.jpg";
 
 export default function Home() {
-  const [modal, setModal] = useState(false);
+  const [backgound, setBackground] = useState(false);
 
   return (
     <div className="min-h-screen flex justify-center md:items-center pt-14 md:pt-0">
@@ -13,30 +13,29 @@ export default function Home() {
         <h1 className="text-7xl">你好，</h1>
         <h2 className="text-4xl">我是Jonny Jin，常用网名银河猫猫侠。</h2>
         <p className="text-xl">
-          本人现在在加拿大读大学，主要从事机器学习相关科研工作，同时也热衷于编程语言。
+          本人现在在加拿大读大学，主要从事机器学习相关工作，同时也热衷于编程语言。
           <PL href="https://exec.felys.dev" text="Felys" />
           是我正在创作的一款编程语言，自娱自乐的同时释放一下我对
           <button
-            className="hover:text-elysia cursor-pointer duration-150"
-            onClick={() => setModal((m) => !m)}
+            className="text-elysia cursor-pointer"
+            onClick={() => setBackground((x) => !x)}
           >
             爱莉希雅
           </button>
-          的厨力，平时闲的话就会继续迭代这个项目。
+          的厨力，是个长期项目。
         </p>
-        <dialog
-          open={modal}
-          className="w-screen h-screen fixed top-0 bg-transparent z-[-1]"
-        >
+        <div className="w-screen h-screen fixed top-0 left-0 bg-transparent z-[-1]">
           <Image
             src={elysia}
             alt=""
             fill
             priority
             placeholder="blur"
-            className="object-cover opacity-5"
+            className={`object-cover ${
+              backgound ? "opacity-5" : "opacity-0"
+            } transition-opacity duration-300`}
           />
-        </dialog>
+        </div>
       </div>
     </div>
   );
