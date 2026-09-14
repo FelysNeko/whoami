16b6484def3c01a67dbd5cddd12d75526fb18cdc
FelysNeko <fake@felys.dev>
2026-09-14 01:02:34 -0400

    patch

--- lib/config/en/index.ts
+++ lib/config/en/index.ts
@@ -10,7 +10,7 @@ export const EN: Config = {
   introduction: "My name is Jonny Jin, also known as FelysNeko.",
   profile: {
     lead: "I am an undergraduate student at the University of Waterloo. My career focus is on machine learning and software development, but I am also interested in compiler principle. The ",
-    tail: " project contains a programming language and a large language model fine-tuning subproject.",
+    tail: " project contains a programming language and two model fine-tuning subprojects.",
   },
   social: {
     lead: "Check out my ",
